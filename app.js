0172d5e20ec8726fb4f3ba5fa23ff53cc3fc2097
tiger24 <146634579+jayagiri14@users.noreply.github.com>
2025-06-29 00:42:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -23,4 +23,7 @@ app.use("/owner", ownerRouter);
 app.use("/product", productRouter);
 app.use("/user", userRouter);
 // app.use("/shop",index)
-app.listen(5000);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
